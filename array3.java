0a4870c10051bbad92e15b2839fba7b9eb50ab84
mohdfaizanusmansait <131851683+mohdfaizanusmansait@users.noreply.github.com>
2023-07-19 20:54:32 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,16 @@
+
+public class array3
+{
+	public static void main(String[] args) {
+	    int arr[]={33,3,4,5};
+	    for(int i:arr)
+	    {
+		System.out.println(i);
+	}
+}
+}
+
+
+
+    
+
---
+++ matrix.java
@@ -0,0 +1,10 @@
+public class matrix {public static void main(String args[]) {
+	int[][] matrix={{2,3,4},{5,6,4},{4,6,9}};
+	for(int[] row:matrix){
+	System.out.println(row+" ");
+	{System.out.println();}
+}
+}
+
+    
+}
